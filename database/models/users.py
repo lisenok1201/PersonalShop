6b9fa1c3075d351d9be5928ecb3c9bdda3d36457
lisenok1201 <bolkunova.alice1244@gmail.com>
2026-02-17 20:12:53 +0300

    Добавление хендлера заказа с инлайн клавиатуры выбора категорий

--- database/models/users.py
+++ database/models/users.py
@@ -14,4 +14,3 @@ class Users(Base):
 
     def __str__(self):
         return self.name
-
--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+
+from config import TOKEN
+from handlers import h01_start, h02_getcontact, h03_order
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+dp.include_router(h01_start.router)
+dp.include_router(h02_getcontact.router)
+dp.include_router(h03_order.router)
 
 
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
