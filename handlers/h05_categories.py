9a12d816ea1fe8dd03e079ad742cee6a41f4c9c9
lisenok1201 <bolkunova.alice1244@gmail.com>
2026-04-28 20:11:05 +0300

    декоратор

--- handlers/h05_categories.py
+++ handlers/h05_categories.py
@@ -2,20 +2,20 @@ from aiogram import Router, F
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.types import CallbackQuery
 
-
 from keyboards.inline import show_product_by_category, generate_category_menu
 
 router = Router()
 
+
 @router.callback_query(F.data.regexp(r'^category_(\d+)$'))
-async def show_product(callback:CallbackQuery):
+async def show_product(callback: CallbackQuery):
     """демонстрация всех продуктов из выбранной категори"""
     chat_id = callback.message.chat.id
     message_id = callback.message.message_id
     category_id = int(callback.data.split("_")[-1])
     try:
         await callback.bot.edit_message_text(
-            text = "Выберите продукт",
+            text="Выберите продукт",
             chat_id=chat_id,
             message_id=message_id,
             reply_markup=show_product_by_category(category_id)
@@ -23,15 +23,16 @@ async def show_product(callback:CallbackQuery):
     except TelegramBadRequest:
         await callback.answer("НЕ удалось открыть выбранную категорию")
 
-@router.callback_query(F.data=='return_to_category')
-async def return_to_category(callback:CallbackQuery):
+
+@router.callback_query(F.data == 'return_to_category')
+async def return_to_category(callback: CallbackQuery):
     '''Возврат к списку категории'''
     chat_id = callback.message.chat.id
     message_id = callback.message.message_id
 
     await callback.bot.edit_message_text(
-        text = 'Выберите категорию',
+        text='Выберите категорию',
         chat_id=chat_id,
         message_id=message_id,
         reply_markup=generate_category_menu(chat_id)
-        )
+    )
