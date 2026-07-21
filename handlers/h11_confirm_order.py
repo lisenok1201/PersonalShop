5c12d91b04d8b8e480e01d7109b4b22c8c956124
lisenok1201 <bolkunova.alice1244@gmail.com>
2026-07-21 20:19:18 +0300

    начали работу над хендлером оформить заказ

--- handlers/h11_confirm_order.py
+++ handlers/h11_confirm_order.py
@@ -1,13 +1,16 @@
 from aiogram import Router, F, Bot
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import CallbackQuery
+
+from database.utils import db_get_user_phone
 
 router = Router()
+
 
 @router.callback_query(F.data=='confirm_order')
 async def confirm_order (callback: CallbackQuery, bot: Bot):
     """Оформлене заказа"""
     user = callback.from_user
-    phone = db_get_user_phone
+    phone = db_get_user_phone(user.id)
 
 
 
